ea2a6daab7c792e6b7827d3ca5082a2d5c432cf9
Hind <newhind2023@gmail.com>
2025-12-17 09:03:30 +0300

    update setting

--- AwareNow_Project/settings.py
+++ AwareNow_Project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
+    'core',
+    'courses',
+    'campaigns',
+    'policies',
 ]
 
 MIDDLEWARE = [
--- AwareNow_Project/urls.py
+++ AwareNow_Project/urls.py
@@ -20,4 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('account.urls')),
+    path('',include('campaigns.urls')),
+    path('',include('courses.urls')),
+    path('',include('policies.urls')),
 ]
